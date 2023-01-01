9132b823e443a347573d378b8dadb4f9e42e916f
Kevin MacPhail <kevinmacphail@yahoo.com>
2023-01-01 18:46:19 -0400

    Update batch file
    Call a powershell script to search the registry to locate the scribus exe

--- generate_NGFD.bat
+++ generate_NGFD.bat
@@ -1,3 +1,5 @@
-scribus -g -ns -py .\ScribusGenerator-3.0\ScribusGeneratorCLI.py -m --outName NGFD_tags .\NGFD.sla
-scribus -g -ns -py .\to-pdf\to-pdf.py -- NGFD_tags.sla
-
+@echo off
+for /f "delims=" %%a in ('powershell /noprofile /executionpolicy bypass /command ".\find_scribus.ps1"') do set "var=%%a"
+@echo on
+"%var%" -g -ns -py .\ScribusGenerator-3.0\ScribusGeneratorCLI.py -m --outName NGFD_tags .\NGFD.sla
+"%var%" -g -ns -py .\to-pdf\to-pdf.py -- NGFD_tags.sla

607b20d1fb1bfbe3ca70cbea9c6bb58bef9b8b96
Bob Shand <bob@shand.ca>
2023-01-02 13:30:15 -0400

    Move the scripts into a dedicated script folder. Update the generateNGFD batch file and gitignore based on the move to scripts.

--- generate_NGFD.bat
+++ generate_NGFD.bat
@@ -1,5 +1,5 @@
 @echo off
-for /f "delims=" %%a in ('powershell /noprofile /executionpolicy bypass /command ".\find_scribus.ps1"') do set "var=%%a"
+for /f "delims=" %%a in ('powershell /noprofile /executionpolicy bypass /command ".\scripts\find_scribus.ps1"') do set "var=%%a"
 @echo on
-"%var%" -g -ns -py .\ScribusGenerator-3.0\ScribusGeneratorCLI.py -m --outName NGFD_tags .\NGFD.sla
-"%var%" -g -ns -py .\to-pdf\to-pdf.py -- NGFD_tags.sla
+"%var%" -g -ns -py .\scripts\ScribusGenerator-3.0\ScribusGeneratorCLI.py -m --outName NGFD_tags .\NGFD.sla
+"%var%" -g -ns -py .\scripts\to-pdf\to-pdf.py -- NGFD_tags.sla
